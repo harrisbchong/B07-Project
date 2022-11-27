a91863860cedeea367492fa4b2656bd802aed91f
JimXYC <114100120+JimXYC@users.noreply.github.com>
2022-11-26 20:26:14 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/b07project/AdminFrontPage.java
@@ -0,0 +1,43 @@
+package com.example.b07project;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.b07project.databinding.FragmentAdminFrontPageBinding;
+import com.example.b07project.databinding.FragmentAdminLoginPageBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+
+public class AdminFrontPage extends Fragment {
+
+    private FragmentAdminFrontPageBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAdminFrontPageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.backButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(AdminFrontPage.this).navigate(R.id.action_adminFrontPage_to_adminLoginPage);
+            }
+        });
+    }
+}
