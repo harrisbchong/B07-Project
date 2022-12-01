9537289db6236801f822ef8ba516a399177c7646
JimXYC <114100120+JimXYC@users.noreply.github.com>
2022-12-01 18:08:30 -0500

    Add files via upload

--- app/src/main/java/com/example/b07project/SCourseViewAdapter.java
+++ app/src/main/java/com/example/b07project/SCourseViewAdapter.java
@@ -3,7 +3,10 @@ package com.example.b07project;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -29,6 +32,12 @@ public class SCourseViewAdapter extends RecyclerView.Adapter<SCourseViewAdapter.
     @Override
     public void onBindViewHolder(MyHolder holder, int position) {
         holder.textView.setText(mList.get(position).toString());
+        holder.ibt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                holder.textView.setText("Testing!!!" + position);
+            }
+        });
     }
 
 
@@ -41,10 +50,12 @@ public class SCourseViewAdapter extends RecyclerView.Adapter<SCourseViewAdapter.
     class MyHolder extends RecyclerView.ViewHolder {
 
         TextView textView;
+        ImageButton ibt;
 
         public MyHolder(View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.tv_content);
+            ibt = itemView.findViewById(R.id.imageButton2);
         }
     }
 }
